6248278358a768edc9807eff483e1b5a17e02d00
kytaec <vladislavtokovenko4@gmail.com>
2024-11-18 15:52:55 +0200

    changed painting cube shape and segment shape

--- app/src/main/java/com/example/lab2/Shapes/CubeShape.kt
+++ app/src/main/java/com/example/lab2/Shapes/CubeShape.kt
@@ -18,16 +18,12 @@ class CubeShape : Shape("Куб"), IRectangleShape, ILineShape {
     val width = endX - startX
     val offset = width / 4
 
-    drawCoordLine(canvas, startX + offset, startY - offset, endX + offset, startY - offset, isDrawing) // Top edge
-    drawCoordLine(canvas, startX + offset, startY - offset, startX + offset, endY - offset, isDrawing) // Left top edge
-    drawCoordLine(canvas, endX + offset, startY - offset, endX + offset, endY - offset, isDrawing) // Right top edge
-    drawCoordLine(canvas, startX + offset, endY - offset, endX + offset, endY - offset, isDrawing) // Bottom edge
-
     drawCoordLine(canvas, startX, startY, startX + offset, startY - offset, isDrawing) // Left vertical
     drawCoordLine(canvas, endX, startY, endX + offset, startY - offset, isDrawing) // Right vertical
     drawCoordLine(canvas, startX, endY, startX + offset, endY - offset, isDrawing) // Bottom left vertical
     drawCoordLine(canvas, endX, endY, endX + offset, endY - offset, isDrawing) // Bottom right vertical
 
+    drawRectangleBase(canvas, startX + offset, startY - offset, endX + offset, endY - offset, cubePaint, isDrawing)
     drawRectangleBase(canvas, startX, startY, endX, endY, cubePaint, isDrawing)
   }
 }
--- app/src/main/java/com/example/lab2/Shapes/Interfaces/IEllipseShape.kt
+++ app/src/main/java/com/example/lab2/Shapes/Interfaces/IEllipseShape.kt
@@ -1,11 +1,18 @@
 package com.example.lab2.Shapes.Interfaces
 
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 
 interface IEllipseShape {
   fun drawCircle(canvas: Canvas, centerX: Float, centerY: Float, radius: Float, circleColor: Int) {
     val finalRadius = if (centerX == 0f) 0f else radius
+
+    val basicCirclePaint: Paint = Paint().apply {
+      color = Color.BLACK
+      style = Paint.Style.STROKE
+      strokeWidth = 5f
+    }
 
     val circlePaint: Paint = Paint().apply {
       color = circleColor
@@ -17,6 +24,12 @@ interface IEllipseShape {
       centerX - finalRadius, centerY - finalRadius,
       centerX + finalRadius, centerY + finalRadius,
       circlePaint
+    )
+
+    canvas.drawOval(
+      centerX - finalRadius, centerY - finalRadius,
+      centerX + finalRadius, centerY + finalRadius,
+      basicCirclePaint
     )
   }
 
